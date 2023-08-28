ce6b2d15795ab2ec2b587b0fc14461a545a69251
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-08-28 15:16:50 +0900

    chore: 프로젝트 생성

---
+++ Karlo/Karlo/App/KarloApp.swift
@@ -0,0 +1,13 @@
+//  Karlo - KarloApp.swift
+//  Created by zhilly on 2023/08/28
+
+import SwiftUI
+
+@main
+struct KarloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Karlo/Karlo/View/ContentView.swift
@@ -0,0 +1,16 @@
+//  Karlo - ContentView.swift
+//  Created by zhilly on 2023/08/28
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Karlo!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
