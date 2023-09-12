038f71c750581c73b13d07de9f80e1f02cc2eb60
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-09-13 02:08:05 +0900

    feat: API Constant 구현

---
+++ Karlo/Karlo/Network/APIConstant.swift
@@ -0,0 +1,35 @@
+//  Karlo - APIConstant.swift
+//  Created by zhilly on 2023/09/12
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+enum HTTPHeader {
+    case contentType
+    case authorization(key: String)
+    
+    var field: String {
+        switch self {
+        case .contentType:
+            return "Content-Type"
+        case .authorization:
+            return "Authorization"
+        }
+    }
+    
+    var value: String {
+        switch self {
+        case .contentType:
+            return "application/json"
+        case .authorization(let apikey):
+            return "KakaoAK \(apikey)"
+        }
+    }
+}
+
