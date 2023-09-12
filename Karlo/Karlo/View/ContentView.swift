7db05c4f8464f062c2db85fc0eae94ecb274226a
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-09-13 02:13:27 +0900

    feat: Main 화면 구조 수정

--- Karlo/Karlo/View/ContentView.swift
+++ Karlo/Karlo/View/ContentView.swift
@@ -20,106 +20,109 @@ struct ContentView: View {
     private var imageSizes: [Int] = [384, 512, 640]
     
     var body: some View {
-        VStack {
-            Form {
-                Section("긍정 제시어 입력") {
-                    KeywordInputView(isErrorKeyword: $isErrorKeyword, keywordError: $keywordError)
-                        .environmentObject(positiveKeywordViewModel)
-                }
-                
-                Section("긍정 제시어") {
-                    KeywordTagView()
-                        .environmentObject(positiveKeywordViewModel)
-                }
-                
-                Section("부정 제시어 입력") {
-                    KeywordInputView(isErrorKeyword: $isErrorKeyword, keywordError: $keywordError)
-                        .environmentObject(negativeKeywordViewModel)
-                }
-                
-                Section("부정 제시어") {
-                    KeywordTagView()
-                        .environmentObject(negativeKeywordViewModel)
-                }
-                
-                Section("이미지 사이즈(단위: 픽셀)") {
-                    VStack(alignment: .leading) {
-                        Text("너비")
-                            .font(.caption)
-                        
-                        Picker("너비 사이즈", selection: $imageWidth) {
-                            ForEach(imageSizes, id: \.self) {
-                                Text("\($0)")
-                            }
-                        }
-                        .pickerStyle(.segmented)
+        NavigationStack {
+            VStack {
+                Form {
+                    Section("긍정 제시어 입력") {
+                        KeywordInputView(isErrorKeyword: $isErrorKeyword, keywordError: $keywordError)
+                            .environmentObject(positiveKeywordViewModel)
                     }
                     
-                    VStack(alignment: .leading) {
-                        Text("높이")
-                            .font(.caption)
+                    Section("긍정 제시어") {
+                        KeywordTagView()
+                            .environmentObject(positiveKeywordViewModel)
+                    }
+                    
+                    Section("부정 제시어 입력") {
+                        KeywordInputView(isErrorKeyword: $isErrorKeyword, keywordError: $keywordError)
+                            .environmentObject(negativeKeywordViewModel)
+                    }
+                    
+                    Section("부정 제시어") {
+                        KeywordTagView()
+                            .environmentObject(negativeKeywordViewModel)
+                    }
+                    
+                    Section("이미지 사이즈(단위: 픽셀)") {
+                        VStack(alignment: .leading) {
+                            Text("너비")
+                                .font(.caption)
+                            
+                            Picker("너비 사이즈", selection: $imageWidth) {
+                                ForEach(imageSizes, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
                         
-                        Picker("높이 사이즈", selection: $imageHeight) {
-                            ForEach(imageSizes, id: \.self) {
-                                Text("\($0)")
+                        VStack(alignment: .leading) {
+                            Text("높이")
+                                .font(.caption)
+                            
+                            Picker("높이 사이즈", selection: $imageHeight) {
+                                ForEach(imageSizes, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                    }
+                    
+                    Section("이미지 확대 여부") {
+                        Toggle(isOn: $imageScale) {
+                            Text("확대")
+                        }
+                        .tint(Color.blue)
+                    }
+                    
+                    Section("이미지 저장 품질(기본값: 70)") {
+                        VStack {
+                            HStack {
+                                Text("품질: \(Int(imageQuality))")
+                            }
+                            
+                            Slider(
+                                value: $imageQuality,
+                                in: 1...100,
+                                step: 1
+                            ) {
+                                Text("품질")
+                            } minimumValueLabel: {
+                                Text("1")
+                            } maximumValueLabel: {
+                                Text("100")
                             }
                         }
-                        .pickerStyle(.segmented)
                     }
-                }
-                
-                Section("이미지 확대 여부") {
-                    Toggle(isOn: $imageScale) {
-                        Text("확대")
-                    }
-                    .tint(Color.blue)
-                }
-                
-                Section("이미지 저장 품질(기본값: 70)") {
-                    VStack {
-                        HStack {
-                            Text("품질: \(Int(imageQuality))")
-                        }
-                        
-                        Slider(
-                            value: $imageQuality,
-                            in: 1...100,
-                            step: 1
-                        ) {
-                            Text("품질")
-                        } minimumValueLabel: {
-                            Text("1")
-                        } maximumValueLabel: {
-                            Text("100")
+                    
+                    Section("생성할 이미지 수(기본값: 1, 최대: 8)") {
+                        Stepper(value: $imageCount,
+                                in: 1...8,
+                                step: 1) {
+                            Text("이미지 수: \(imageCount)")
                         }
                     }
-                }
-                
-                Section("생성할 이미지 수(기본값: 1, 최대: 8)") {
-                    Stepper(value: $imageCount,
-                            in: 1...8,
-                            step: 1) {
-                        Text("이미지 수: \(imageCount)")
-                    }
-                }
-                
-                Section {
-                    Button(action: {
-                        self.isAlbumViewPresented.toggle()
-                    }) {
-                        Text("이미지 생성하기")
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                    }
-                    .listRowInsets(EdgeInsets())
-                    .sheet(isPresented: $isAlbumViewPresented) {
-                        AlbumView(imageInfo: makeImageInfo())
+                    
+                    Section {
+                        Button(action: {
+                            self.isAlbumViewPresented.toggle()
+                        }) {
+                            Text("이미지 생성하기")
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        .listRowInsets(EdgeInsets())
+                        .sheet(isPresented: $isAlbumViewPresented) {
+                            AlbumView(imageInfo: makeImageInfo())
+                        }
                     }
                 }
             }
+            .navigationTitle("이미지 생성하기")
         }
     }
     
