306e2a3a16d92e58fe8114c2143e164b4de16b25
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-10-10 17:10:05 +0900

    test: KeywordTagFeature test case 추가

--- Karlo/KarloTests/KeywordTagFeatureTests.swift
+++ Karlo/KarloTests/KeywordTagFeatureTests.swift
@@ -10,6 +10,17 @@ import XCTest
 final class KeywordTagFeatureTests: XCTestCase {
     func testAddWrongKeywordTag() async {
         let testWrongPrompt: String = "안녕하세요"
+        let emptyPrompt: String = ""
+        let limitOverPrompt: String = """
+            portrait of beautiful young woman, ((by Shilin Huang, photorealistic anime girl
+            render, artgeem)), highly detailed face, digital art, impressionism,deep gaze, princess
+            eyes, long golden blonde straight hair, straight hair, light makeup, gorgeous white and
+            gold wedding dress, digital painting, sharp features, sharp focus, face centered,
+            detailed eyes, British, illustration, kids-book, nature, playful, soft face, bokeh,
+            soft lighting, bright light stained glass windows background, emotional, medieval
+            fantasy, frontal gaze, romance fantasy webtoon, ((distant view)), best dynamic
+            composition, ((Front face photo))
+            """
         
         let store = TestStore(initialState: KeywordTagFeature.State()) {
             KeywordTagFeature()
@@ -24,6 +35,34 @@ final class KeywordTagFeatureTests: XCTestCase {
         await store.send(.addTag) {
             $0.alert = AlertState {
                 TextState(KeywordError.containHangul.localizedDescription)
+            } actions: {
+                ButtonState(role: .cancel) {
+                    TextState(Constant.Text.check)
+                }
+            }
+        }
+        
+        await store.send(.textChanged(emptyPrompt)) {
+            $0.tagStore.tagText = emptyPrompt
+        }
+        
+        await store.send(.addTag) {
+            $0.alert = AlertState {
+                TextState(KeywordError.emptyKeyword.localizedDescription)
+            } actions: {
+                ButtonState(role: .cancel) {
+                    TextState(Constant.Text.check)
+                }
+            }
+        }
+        
+        await store.send(.textChanged(limitOverPrompt)) {
+            $0.tagStore.tagText = limitOverPrompt
+        }
+        
+        await store.send(.addTag) {
+            $0.alert = AlertState {
+                TextState(KeywordError.overRange.localizedDescription)
             } actions: {
                 ButtonState(role: .cancel) {
                     TextState(Constant.Text.check)
